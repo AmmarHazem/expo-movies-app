3d385bc3f0563a4723d72a7530a4918d9d624448
Ammar <ammar@ovasave.com>
2025-04-19 17:20:33 +0400

    setting up native wind

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-red-600 text-white flex-1 items-center justify-center">
+      <Text className="text-white">Hello</Text>
+    </View>
+  );
+}
