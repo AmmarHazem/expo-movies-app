1c17060710d91eb993b4b6fae84a4738e16ea731
Ammar <ammar@ovasave.com>
2025-04-26 14:40:43 +0400

    setup app icon and splash screen

--- src/app/(home-tabs)/_layout.tsx
+++ src/app/(home-tabs)/_layout.tsx
@@ -1,33 +1,70 @@
 import { icons } from "@/constants/icons";
-import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
-import { ImageBackground, Image, Text } from "react-native";
+import TabIcon from "@/src/component/TabIcon";
 
 // https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg
 
 export default function HomeTabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0F0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0F0D23",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           tabBarIcon: (props) => {
-            return (
-              <ImageBackground
-                source={images.highlight}
-                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-              >
-                <Image src={icons.home} tintColor={"#151312"} className="size-5" />
-                <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
-              </ImageBackground>
-            );
+            return <TabIcon focused={props.focused} title="Home" icon={icons.home} />;
           },
         }}
       />
-      <Tabs.Screen name="search" options={{ title: "Search" }} />
-      <Tabs.Screen name="saved" options={{ title: "Saved" }} />
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: (props) => {
+            return <TabIcon focused={props.focused} title="Search" icon={icons.search} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="saved"
+        options={{
+          title: "Saved",
+          tabBarIcon: (props) => {
+            return <TabIcon focused={props.focused} title="Saved" icon={icons.save} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: (props) => {
+            return <TabIcon focused={props.focused} title="Profile" icon={icons.person} />;
+          },
+        }}
+      />
     </Tabs>
   );
 }
