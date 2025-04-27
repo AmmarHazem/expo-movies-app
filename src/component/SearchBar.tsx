f1a0e9ba6f37e4e0484c3499bb41cb9747797b60
Ammar <ammar@ovasave.com>
2025-04-27 21:07:33 +0400

    working on search bar

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,11 +1,19 @@
 import { icons } from "@/constants/icons";
 import { FC } from "react";
-import { Image, Text } from "react-native";
+import { Image, Text, TextInput } from "react-native";
 
 const SearchBar: FC = () => {
   return (
-    <Text className="flex-row items-center bg-dark-200 rounded-full px-4 py-4">
+    <Text className="items-center flex-row bg-green-600 rounded-full px-4 py-0">
       <Image source={icons.search} className="size-5" resizeMode="contain" tintColor={"#AB8BFF"} />
+      <TextInput
+        className="flex-1 h-10 ml-2 text-white bg-red-600"
+        placeholder="Search"
+        // value=""
+        // onPress={() => {}}
+        // onChangeText={() => {}}
+        placeholderTextColor={"#A8B5DB"}
+      />
     </Text>
   );
 };
