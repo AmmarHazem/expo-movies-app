fd7d2fdb722e0f55bc7f0f50183941db71ade18c
Ammar <ammar@ovasave.com>
2025-04-28 20:52:50 +0400

    setting up swr

--- src/app/(home-tabs)/index.tsx
+++ src/app/(home-tabs)/index.tsx
@@ -2,9 +2,12 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { FC } from "react";
 import { Image, ScrollView, View } from "react-native";
+import { useRouter } from "expo-router";
 import SearchBar from "@/src/component/SearchBar";
 
 const HomeScreen: FC = () => {
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute w-full z-0 top-0" />
@@ -15,7 +18,7 @@ const HomeScreen: FC = () => {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-4 mx-auto" />
         <View className="flex-1">
-          <SearchBar />
+          <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie" />
         </View>
       </ScrollView>
     </View>
--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,21 +1,29 @@
 import { icons } from "@/constants/icons";
 import { FC } from "react";
-import { Image, Text, TextInput } from "react-native";
+import { Image, View, TextInput, Pressable, Text } from "react-native";
 
-const SearchBar: FC = () => {
+const SearchBar: FC<SearchBarProps> = ({ onPress, placeholder }) => {
   return (
-    <Text className="items-center flex-row bg-green-600 rounded-full px-4 py-0">
-      <Image source={icons.search} className="size-5" resizeMode="contain" tintColor={"#AB8BFF"} />
-      <TextInput
-        className="flex-1 h-10 ml-2 text-white bg-red-600"
-        placeholder="Search"
-        // value=""
-        // onPress={() => {}}
-        // onChangeText={() => {}}
-        placeholderTextColor={"#A8B5DB"}
-      />
-    </Text>
+    <Pressable onPress={onPress}>
+      <View className="w-full gap-4 flex-row items-center h-12 bg-dark-200 rounded-full px-4 py-0">
+        <Image source={icons.search} className="size-5" resizeMode="cover" tintColor={"#AB8BFF"} />
+        <Text style={{ color: "white" }}>{placeholder ?? "Search"}</Text>
+        {/* <TextInput
+          className="flex-1 h-full ms-2 text-white"
+          placeholder={placeholder ?? "Search"}
+          // value=""
+          onPress={onPress}
+          // onChangeText={() => {}}
+          placeholderTextColor={"#A8B5DB"}
+        /> */}
+      </View>
+    </Pressable>
   );
 };
+
+interface SearchBarProps {
+  onPress?: () => void;
+  placeholder?: string;
+}
 
 export default SearchBar;
